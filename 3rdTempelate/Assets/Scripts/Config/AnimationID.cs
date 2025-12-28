1d13373368900c681cd8fced40b44c342ed4c011
Mint-Hans <2718663285@qq.com>
2025-12-28 17:32:15 +0800

    Attack_Try_0.1

--- 3rdTempelate/Assets/Scripts/Config/AnimationID.cs
+++ 3rdTempelate/Assets/Scripts/Config/AnimationID.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Config
 {
-    public class AnimationID 
+    public class AnimationID
     {
         public static readonly int Movement = Animator.StringToHash("Movement");
         public static readonly int Lock = Animator.StringToHash("Lock");
@@ -15,6 +15,9 @@ namespace Config
         public static readonly int Die = Animator.StringToHash("Die");
         public static readonly int Parry = Animator.StringToHash("Parry");
         public static readonly int Roll = Animator.StringToHash("Roll");
-    
+        public static readonly int LAttack = Animator.StringToHash("LAttack"); // 左键攻击触发
+        public static readonly int RAttack = Animator.StringToHash("RAttack"); // 右键攻击触发
+        public static readonly int ComboStep = Animator.StringToHash("ComboStep"); // 连击段数
+        public static readonly int IsAttacking = Animator.StringToHash("IsAttacking"); // 是否在攻击中 
     }
 }
